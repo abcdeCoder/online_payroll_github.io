704069be86bb1405ef814f73d4cc778ac21cdaf8
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-07-01 19:00:15 +0530

    Update payroll.php

--- admin/payroll.php
+++ admin/payroll.php
@@ -27,31 +27,31 @@ $range_from = date('m/d/Y', strtotime('-30 day', strtotime($range_to)));
             <!-- Main content -->
             <section class="content">
                 <?php
-                if (isset($_SESSION['error'])) {
-                    echo "
+      if (isset($_SESSION['error'])) {
+          echo "
             <div class='alert alert-danger alert-dismissible'>
               <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
               <h4><i class='icon fa fa-warning'></i> Error!</h4>
               " .
-                        $_SESSION['error'] .
-                        "
+              $_SESSION['error'] .
+              "
             </div>
           ";
-                    unset($_SESSION['error']);
-                }
-                if (isset($_SESSION['success'])) {
-                    echo "
+          unset($_SESSION['error']);
+      }
+      if (isset($_SESSION['success'])) {
+          echo "
             <div class='alert alert-success alert-dismissible'>
               <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
               <h4><i class='icon fa fa-check'></i> Success!</h4>
               " .
-                        $_SESSION['success'] .
-                        "
+              $_SESSION['success'] .
+              "
             </div>
           ";
-                    unset($_SESSION['success']);
-                }
-                ?>
+          unset($_SESSION['success']);
+      }
+      ?>
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="box">
@@ -64,12 +64,10 @@ $range_from = date('m/d/Y', strtotime('-30 day', strtotime($range_to)));
                                             </div>
                                             <input type="text" class="form-control pull-right col-sm-8" id="reservation"
                                                 name="date_range" value="<?php echo isset(
-                                                    $_GET['range']
-                                                )
-                                                    ? $_GET['range']
-                                                    : $range_from .
-                                                        ' - ' .
-                                                        $range_to; ?>">
+                        $_GET['range']
+                    )
+                        ? $_GET['range']
+                        : $range_from . ' - ' . $range_to; ?>">
                                         </div>
                                         <button type="button" class="btn btn-success btn-sm btn-flat" id="payroll"><span
                                                 class="glyphicon glyphicon-print"></span> Payroll</button>
@@ -90,67 +88,57 @@ $range_from = date('m/d/Y', strtotime('-30 day', strtotime($range_to)));
                                     </thead>
                                     <tbody>
                                         <?php
-                                        $sql = 'SELECT * FROM deductions';
-                                        $query = $conn->query($sql);
-                                        $drow = $query->fetch_assoc();
-                                        $deduction = $drow['total_amount'];
-                                        $to = date('Y-m-d');
-                                        $from = date(
-                                            'Y-m-d',
-                                            strtotime('-30 day', strtotime($to))
-                                        );
-                                        if (isset($_GET['range'])) {
-                                            $range = $_GET['range'];
-                                            $ex = explode(' - ', $range);
-                                            $from = date(
-                                                'Y-m-d',
-                                                strtotime($ex[0])
-                                            );
-                                            $to = date(
-                                                'Y-m-d',
-                                                strtotime($ex[1])
-                                            );
-                                        }
-                                        $sql = 'SELECT * FROM attendance';
-                                        $query = $conn->query($sql);
-                                        $total = 0;
-                                        while ($row = $query->fetch_assoc()) {
-                                            $empid = $row['empid'];
-                                            $casql = "SELECT * FROM cashadvance WHERE employee_id='$empid' AND date_advance BETWEEN '$from' AND '$to'";
-                                            $caquery = $conn->query($casql);
-                                            $carow = $caquery->fetch_assoc();
-                                            $cashadvance = $carow['cashamount'];
-                                            $gross =
-                                                $row['rate'] * $row['total_hr'];
-                                            $total_deduction =
-                                                $deduction + $cashadvance;
-                                            $net = $gross - $total_deduction;
-                                            echo "
+                  $sql =
+                      'SELECT *, SUM(amount) as total_amount FROM deductions';
+                  $query = $conn->query($sql);
+                  $drow = $query->fetch_assoc();
+                  $deduction = $drow['total_amount'];
+                  $to = date('Y-m-d');
+                  $from = date('Y-m-d', strtotime('-30 day', strtotime($to)));
+                  if (isset($_GET['range'])) {
+                      $range = $_GET['range'];
+                      $ex = explode(' - ', $range);
+                      $from = date('Y-m-d', strtotime($ex[0]));
+                      $to = date('Y-m-d', strtotime($ex[1]));
+                  }
+                  $sql = "SELECT *, SUM(num_hr) AS total_hr, attendance.employee_id AS empid FROM attendance LEFT JOIN employees ON employees.id=attendance.employee_id LEFT JOIN position ON position.id=employees.position_id WHERE date BETWEEN '$from' AND '$to' GROUP BY attendance.employee_id ORDER BY employees.lastname ASC, employees.firstname ASC";
+                  $query = $conn->query($sql);
+                  $total = 0;
+                  while ($row = $query->fetch_assoc()) {
+                      $empid = $row['empid'];
+                      $casql = "SELECT *, SUM(amount) AS cashamount FROM cashadvance WHERE employee_id='$empid' AND date_advance BETWEEN '$from' AND '$to'";
+                      $caquery = $conn->query($casql);
+                      $carow = $caquery->fetch_assoc();
+                      $cashadvance = $carow['cashamount'];
+                      $gross = $row['rate'] * $row['total_hr'];
+                      $total_deduction = $deduction + $cashadvance;
+                      $net = $gross - $total_deduction;
+                      echo "
                         <tr>
                           <td>" .
-                                                $row['lastname'] .
-                                                ', ' .
-                                                $row['firstname'] .
-                                                "</td>
+                          $row['lastname'] .
+                          ', ' .
+                          $row['firstname'] .
+                          "</td>
                           <td>" .
-                                                $row['employee_id'] .
-                                                "</td>
+                          $row['employee_id'] .
+                          "</td>
                           <td>" .
-                                                number_format($gross, 2) .
-                                                "</td>
+                          number_format($gross, 2) .
+                          "</td>
                           <td>" .
-                                                number_format($deduction, 2) .
-                                                "</td>
+                          number_format($deduction, 2) .
+                          "</td>
                           <td>" .
-                                                number_format($cashadvance, 2) .
-                                                "</td>
+                          number_format($cashadvance, 2) .
+                          "</td>
                           <td>" .
-                                                number_format($net, 2) .
-                                                "</td>
+                          number_format($net, 2) .
+                          "</td>
                         </tr>
                       ";
-                                        }
-                                        ?>
+                  }
+                  ?>
                                     </tbody>
                                 </table>
                             </div>
