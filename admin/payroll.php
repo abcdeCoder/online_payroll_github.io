00f58360eabc02b9390d1687f9bd6e9adaad393b
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-07-01 18:15:40 +0530

    Update payroll.php

--- admin/payroll.php
+++ admin/payroll.php
@@ -117,7 +117,8 @@ $range_from = date('m/d/Y', strtotime('-30 day', strtotime($range_to)));
                                         $total = 0;
                                         while ($row = $query->fetch_assoc()) {
                                             $empid = $row['empid'];
-                                            $casql = "SELECT *, SUM(amount) AS cashamount FROM cashadvance WHERE employee_id='$empid' AND date_advance BETWEEN '$from' AND '$to'";
+                                            $casql =
+                                                'SELECT * FROM cashadvance';
                                             $caquery = $conn->query($casql);
                                             $carow = $caquery->fetch_assoc();
                                             $cashadvance = $carow['cashamount'];
