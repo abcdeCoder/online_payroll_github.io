3f47e3eda1c95270e8b754a28828bd6453b7d0f4
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-07-03 16:45:20 +0530

    Update payroll.php

--- admin/payroll.php
+++ admin/payroll.php
@@ -92,7 +92,7 @@ $range_from = date('m/d/Y', strtotime('-30 day', strtotime($range_to)));
                                         <?php
                                         $dsql =
                                             'SELECT  SUM(amount) as total_amount FROM deductions';
-                                        $dquery = $dconn->query($dsql);
+                                        $dquery = $conn->query($dsql);
                                         $drow = $dquery->fetch_assoc();
                                         $deduction = $drow['total_amount'];
                                         $to = date('Y-m-d');
