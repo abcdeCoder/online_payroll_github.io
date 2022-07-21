37731cd9803a69c3899196998278c093f7b4cb34
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-07-21 12:27:35 +0530

    Update conn.php

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('sql6.freesqldatabase.com', 'sql6505184', 'ZYbQD4LtuE', 'sql6505184');
+$conn = new mysqli('sql6.freesqldatabase.com', 'sql6507794', 'RKR8zmYwEL', 'sql6507794');
 
 if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
